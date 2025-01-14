3d0c965e05a923f06f849c7cc05082b1534eeb07
Ka Ka Lưu <kacode357@gmail.com>
2025-01-14 19:21:14 +0700

    update project v1.0.1

--- src/utils/validateIpnSignature.js
+++ src/utils/validateIpnSignature.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-const config = require("config");
+const config = require("../config/default.json");
 
 function validateIpnSignature(query) {
   const { vnp_SecureHash, ...restParams } = query;
@@ -14,9 +14,14 @@ function validateIpnSignature(query) {
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
 
+  const secretKey = config.vnp_HashSecret; // Đảm bảo không undefined
+  if (!secretKey) {
+    throw new Error("vnp_HashSecret is not defined in configuration");
+  }
+
   const hash = crypto
-    .createHmac("sha512", config.vnp_HashSecret)
-    .update(querystring)
+    .createHmac("sha512", secretKey)
+    .update(Buffer.from(querystring, "utf-8"))
     .digest("hex");
 
   return hash === vnp_SecureHash;
