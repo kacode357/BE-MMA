8de7a3496c7d4ef972143cc28bc9c4c39e933d4a
Ka Ka Lưu <kacode357@gmail.com>
2025-01-15 12:18:41 +0700

    update project v1.0.1

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,6 @@
-const AuthRoute = require("./auth.route");
-const UserRoute = require("./user.route");
-
+// const AuthRoute = require("./auth.route");
+// const UserRoute = require("./user.route");
+const CheckApi = require("./checkapi.route");
 const FoodRoute = require("./food.route");
 const CartRoute = require("./cart.route");
 const Payment = require("./payment.route");
@@ -9,9 +9,7 @@ const VnPay = require("./vnpay.route");
 
 
 const initRoute = (app) => {
-  app.use("/v1/api", AuthRoute);
-  app.use("/v1/api", UserRoute);
-
+  app.use("", CheckApi);
   app.use("/v1/api", FoodRoute);
   app.use("/v1/api", CartRoute);
   app.use("/api", Payment);
