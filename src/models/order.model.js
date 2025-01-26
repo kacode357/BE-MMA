3264de4f0561b793730e331741aac53af3786411
Ka Ka Lưu <kacode357@gmail.com>
2025-01-26 11:27:55 +0700

    update version 1.0.8

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
+    order_id: {
+      type: String,
+      unique: true, // Đảm bảo giá trị không trùng lặp
+      default: () => new mongoose.Types.ObjectId().toString(), // Tự động tạo giá trị duy nhất nếu không được cung cấp
+    },
     items: [
       {
         food_id: { type: mongoose.Schema.Types.ObjectId, ref: "Food", required: true },
@@ -10,9 +15,17 @@ const orderSchema = new mongoose.Schema(
       },
     ],
     total_price: { type: Number, required: true },
-    status: { type: String, enum: ["pending", "completed", "cancelled"], default: "pending" },
+    status: {
+      type: String,
+      enum: ["pending", "completed", "cancelled"],
+      default: "pending",
+    },
     is_paid: { type: Boolean, default: false },
-    created_by: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Nhân viên tạo đơn
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true, // Đảm bảo luôn có người tạo đơn
+    }, // Nhân viên tạo đơn
   },
   { timestamps: true }
 );
