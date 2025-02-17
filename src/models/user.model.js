91d8ee97b77f44ec847a686432ffe988c618c5bc
Ka Ka Lưu <kacode357@gmail.com>
2025-02-17 14:12:08 +0700

    update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, unique: true, required: true },
+    fullname: { type: String, default: null },
     password: { type: String, required: true },
     role: { type: String, enum: ["admin", "staff"], default: "staff" },
-    image: { type: String, default: "" },
+    image: { type: String, default: null },
     is_active: { type: Boolean, default: true },
     locations: [
       {
