03de86337269ed199ff8f26bdcc05b692a7ddffc
Ka Ka Lưu <kacode357@gmail.com>
2025-05-22 13:16:22 +0700

    update new version 2.0

--- src/routes/package.route.js
+++ src/routes/package.route.js
@@ -3,7 +3,7 @@ const PackageController = require("../controllers/package.controller");
 const router = express.Router();
 
 router.post("/packages", PackageController.createPackageController);
-router.get("/packages/:id/access", PackageController.checkPackageAccessController);
+router.post("/packages/:id/access", PackageController.checkPackageAccessController);
 router.post("/packages/search", PackageController.getAllPackagesController); 
 router.get("/packages/:id", PackageController.getPackageByIdController);
 router.put("/packages/:id", PackageController.updatePackageController);
